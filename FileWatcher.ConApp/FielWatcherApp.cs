b7e8398276921813feabddffc99f7b8c8100a1b1
ggehrer <g.gehrer@hotmail.com>
2024-04-23 19:23:39 +0200

    Create project

---
+++ FileWatcher.ConApp/FielWatcherApp.cs
@@ -0,0 +1,101 @@
+﻿//@BaseCode
+//MdStart
+namespace FileWatcher.ConApp
+{
+    /// <summary>
+    /// Represents the main application class for the ToolSet application.
+    /// </summary>
+    internal partial class FileWatcherApp : CommonTool.ConsoleApplication
+    {
+        #region Class-Constructors
+        /// <summary>
+        /// Initializes the <see cref="Program"/> class.
+        /// This static constructor sets up the necessary properties for the program.
+        /// </remarks>
+        static FileWatcherApp()
+        {
+            ClassConstructing();
+            ClassConstructed();
+        }
+        /// <summary>
+        /// This method is called during the construction of the class.
+        /// </summary>
+        static partial void ClassConstructing();
+        /// <summary>
+        /// Represents a method that is called when a class is constructed.
+        /// </summary>
+        static partial void ClassConstructed();
+        #endregion Class-Constructors
+
+        #region Instance-Constructors
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DocConversionApp"/> class.
+        /// </summary>
+        public FileWatcherApp()
+        {
+            Constructing();
+            Constructed();
+        }
+        /// <summary>
+        /// This method is called during the construction of the object.
+        /// </summary>
+        partial void Constructing();
+        /// <summary>
+        /// This method is called when the object is constructed.
+        /// </summary>
+        partial void Constructed();
+        #endregion Instance-Constructors
+
+        #region app properties
+        #endregion app properties
+
+        #region overrides
+        /// <summary>
+        /// Creates an array of menu items for the application menu.
+        /// </summary>
+        /// <returns>An array of MenuItem objects representing the menu items.</returns>
+        protected override MenuItem[] CreateMenuItems()
+        {
+            var mnuIdx = 0;
+            var menuItems = new List<MenuItem>
+            {
+                CreateMenuSeparator(),
+                new()
+                {
+                    Key = $"{++mnuIdx}",
+                    Text = ToLabelText("PlantUML-Watcher", "Watches a folder for changes UML-Diagrams."),
+                    Action = (self) => { new UMLWatcherApp().Run([]); },
+                },
+                new()
+                {
+                    Key = $"{++mnuIdx}",
+                    Text = ToLabelText("Source-Watcher", "Watches a folder for changes source-files."),
+                    Action = (self) => { new SourceWatcherApp().Run([]); },
+                },
+            };
+            return [.. menuItems.Union(CreateExitMenuItems())];
+        }
+
+        /// <summary>
+        /// Prints the header for the PlantUML application.
+        /// </summary>
+        /// <param name="sourcePath">The path of the solution.</param>
+        protected override void PrintHeader()
+        {
+            var count = 0;
+            var saveForeColor = ForegroundColor;
+
+            ForegroundColor = ConsoleColor.Green;
+
+            count = PrintLine(nameof(FileWatcherApp));
+            PrintLine('=', count);
+            PrintLine();
+            ForegroundColor = saveForeColor;
+        }
+        #endregion overrides
+
+        #region app methods
+        #endregion app methods
+    }
+}
+//MdEnd
